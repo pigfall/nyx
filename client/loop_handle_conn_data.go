75b3937b1ddebd5ca0499fe2f84f2d7ece76ce3b
tzz <koi_town@outlook.com>
2021-10-08 13:52:46 +0000

    Add: query ip

--- client/loop_handle_conn_data.go
+++ client/loop_handle_conn_data.go
@@ -1,6 +1,7 @@
 package client
 
 import(
+	"encoding/json"
 	"fmt"
 		"context"
 ws "github.com/gorilla/websocket"
@@ -9,6 +10,7 @@ ws "github.com/gorilla/websocket"
 	"github.com/pigfall/tzzGoUtil/net"
  yy "github.com/pigfall/yingying"
   "github.com/pigfall/yingying/transport"
+  "github.com/pigfall/yingying/proto"
 )
 
 
@@ -21,6 +23,11 @@ func handleConnData(
 	tp := transport.NewTPWebSocket(conn)
 	logger := log.NewHelper("handleConnData",rawLogger,log.LevelDebug)
 	logger.Info("Reading msg from server")
+	ctxQueryIp,cancelQueryIp:=context.WithCancel(ctx)
+	defer cancelQueryIp()
+	go func(){
+		tickerQueryIp(ctxQueryIp,tp,logger)
+	}()
 	for {
 		msgType,data,err := tp.Read()
 		if err != nil{
@@ -38,7 +45,7 @@ func handleConnData(
 				logger.Error(err)
 			}
 		case yy.Proto:
-			handleConnProto(data,clientIp,tunIfce)
+			handleConnProto(data,logger,&clientIp,tunIfce,cancelQueryIp)
 		default:
 			panic(fmt.Errorf("Undefined msgType %v",msgType))
 		}
@@ -50,6 +57,37 @@ func handleIpPacket(data []byte,tunIfce net.TunIfce)error{
 	return err
 }
 
-func handleConnProto(data []byte,clientIp *net.IpWithMask,tunIfce net.TunIfce){
-
+func handleConnProto(data []byte,logger log.LoggerLite,clientIp **net.IpWithMask,tunIfce net.TunIfce,cancelQueryIp func()){
+	var msg proto.Msg
+	err := json.Unmarshal(data,&msg)
+	if err != nil{
+		logger.Error(err)
+		return 
+	}
+	var unmarshalBody = json.Unmarshal
+	switch msg.Id{
+	case proto.ID_S2C_QUERY_IP:
+		var body proto.S2C_ClientVPNIpNet
+		err := unmarshalBody(msg.Body,&body)
+		if err != nil{
+			logger.Error(err)
+			return
+		}
+		ipNet,err := net.FromIpSlashMask(body.IpNet)
+		if err != nil{
+			logger.Error(err)
+			return
+		}
+		if *clientIp == nil{
+			logger.Info("Get Client ip ",ipNet.String())
+			*clientIp = ipNet
+			cancelQueryIp()
+		}else{
+			if (*clientIp).String()!=ipNet.String(){
+				panic("Client ip not match")
+			}
+		}
+	default:
+		panic(fmt.Errorf("Undefined msg id",msg.Id))
+	}
 }
--- client/ticker_query_ip.go
+++ client/ticker_query_ip.go
@@ -1,6 +1,32 @@
 package client
 
+import(
+	"fmt"
+	ctxLib "github.com/pigfall/tzzGoUtil/ctx"
+	"github.com/pigfall/tzzGoUtil/log"
+ yy "github.com/pigfall/yingying"
+ "github.com/pigfall/yingying/proto"
+	"context"
+	"time"
+)
 
 
-func tickerQueryIp(ctx context.Context){
+
+func tickerQueryIp(ctx context.Context,tp yy.Transport,logger log.LoggerLite){
+	ctxLib.TickerDo(ctx,time.Second*2,func()error{
+		err := tp.WriteMsg(
+			&proto.Msg{
+				Id:proto.ID_C2S_QUERY_IP,
+			},
+			nil,
+		)
+		if err != nil{
+			err = fmt.Errorf("Query ip failed %w",err)
+			logger.Error(err)
+			return nil // continue query
+		}
+		logger.Info("Suc send query ip request")
+		return nil
+	})
+	logger.Info("TickerQueryIp quit")
 }
