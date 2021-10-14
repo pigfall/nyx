e5edb6bf816824466d0988396fa5ccb221d8477d
tzz <koi_town@outlook.com>
2021-10-14 15:52:36 +0800

    Add windows route

---
+++ client/ready_tun_route_windows.go
@@ -0,0 +1,15 @@
+package client
+
+
+import(
+	stdnet "net"
+	//"fmt"
+	// "github.com/pigfall/tzzGoUtil/net"
+	"github.com/pigfall/tzzGoUtil/log"
+)
+
+
+func readyTunRoute(logger log.LoggerLite,serverIp stdnet.IP,tunIfceName string)(error){
+	// TODO
+	return nil
+}
