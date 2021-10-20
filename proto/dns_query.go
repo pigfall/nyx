d34d9e667943ad591dc4056085c11af6627598e1
tzz <koi_town@outlook.com>
2021-10-20 14:10:44 +0000

    Add: server dns query

--- proto/dns_query.go
+++ proto/dns_query.go
@@ -5,3 +5,13 @@ type DNSQuery struct{
 	ID string `json:"id"`
 	HostNames []string `json:"hostName"`
 }
+
+type DNSQueryRes struct{
+	ID string
+	Answers []*DNSQueryAnswers
+}
+
+type DNSQueryAnswers struct{
+	HostName string
+	Ips []string
+}
